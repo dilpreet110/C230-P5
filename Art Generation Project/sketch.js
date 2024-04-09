9c427c5a898f426c829a7d828ded6d0a9b95b33d
dilpreet110 <7009873@spsd.sk.ca>
2024-04-09 14:18:09 -0600

    Art Replication

---
+++ Art Generation Project/sketch.js
@@ -0,0 +1,32 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+let amplitude = 20; // Define the amplitude of the wave
+
+function setup() {
+  createCanvas(windowWidth, windowHeight); 
+  strokeWeight(2);
+  // Loop through y values to draw multiple waveforms
+  for (let y = 50; y < height - 50; y += 10) {
+    drawWaveform(y); 
+  }
+}
+
+function drawWaveform(y) {
+  let frequency = 1; // Define the frequency of the wave
+
+  // Loop through x values to draw the waveform
+  for (let x = 20; x < width - 20; x++) {
+    let mappedX = map(x, 20, width - 20, 0, 1.5 * PI); 
+    let mappedY = map(y, 50, height - 50, 0, 89); 
+    let offsetY = amplitude * sin(frequency * mappedX); // Calculate the vertical offset of the wave
+    let waveY = y + offsetY; // Calculate the y-coordinate of the wave
+    point(x, waveY); // Draw a point at the calculated position
+    frequency += 0.01; 
+  }
+}
