87b6846e52cd8c50a3f5e6113a9cee6f283fe7ed
dilpreet110 <7009873@spsd.sk.ca>
2024-04-29 14:24:29 -0600

    1

--- New folder/sketch.js
+++ New folder/sketch.js
@@ -5,33 +5,61 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let grid = 
-[ [0  , 50  , 100 , 150 , 200],
-  [0  ,  0  ,   0 ,   0 ,   0],
-  [30 , 40  , 50  , 60  ,  70],
-  [255, 225 , 255 , 255 , 255]
-
+let grid =
+[ [0,    255,    0,  255,   0],
+  [0,      0,  255,    0,   0],
+  [0,    255,  255,    0,  255],
+  [255,  255,    0,  255,  255]
 ];
 
 let squareSize = 50;
 const NUM_ROWS = 4; const NUM_COLS = 5;
 
+let row, col;
 
 function setup() {
-  createCanvas(NUM_COLS*squareSize, NUM_ROWS*squareSize);
+  createCanvas(NUM_COLS * squareSize, NUM_ROWS * squareSize);
 }
 
 function draw() {
+  col = getCurrentX();
+  row = getCurrentY();  print(col, row);
   background(220);
   drawGrid();
 }
 
+function mousePressed(){
+  flip(col, row);
+}
+
+function flip(x,y){
+  if(grid[y][x]===0) grid[y][x]=255;
+  else grid[y][x] = 0;
+}
+
+function getCurrentY(){
+  //determine current row of mouse, and return
+  let constrainY = constrain(mouseY, 0, height-1);
+  return int(constrainY/squareSize);
+}
+
+function getCurrentX(){
+  //determine the current column of the mouse, and return
+  let constrainX = constrain(mouseX, 0, width-1);
+  return int(constrainX/squareSize);
+}
+
 function drawGrid(){
-  for(let y = 0; y<NUM_ROWS ; y++){
+  // Read data from our 2D Array (grid), and use the 
+  // numbers there to set the color for squares which are
+  // arranged in a grid fashion.
+  for(let y = 0; y<NUM_ROWS; y++){
     for(let x = 0; x<NUM_COLS; x++){
       let fillValue = grid[y][x];
       fill(fillValue);
-      square(x*squareSize,y*squareSize,squareSize);      
-    }
+      //             x:   0 ,   1,     2,    3,     4  
+      //squareSize*x:     0     50    100    150    200
+      square(x*squareSize, y*squareSize, squareSize);
+    }  
   }
 }
