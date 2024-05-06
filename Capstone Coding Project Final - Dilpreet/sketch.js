079bd238d87492263c872603e6d520d6055bdf95
dilpreet110 <7009873@spsd.sk.ca>
2024-05-06 14:21:46 -0600

    Day 1

---
+++ Capstone Coding Project Final - Dilpreet/sketch.js
@@ -0,0 +1,19 @@
+// Capstone Coding Project Final
+// Dilpreet Singh Sidhu
+// 3 May,2024 - Start date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+let paddle; 
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+
